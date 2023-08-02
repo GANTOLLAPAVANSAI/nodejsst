ef637b4912bb59d9fbafadb87d7cefcf8b61bd06
pavan sai <gpavansai0219@gmail.com>
2023-08-02 21:00:34 +0530

    backup nodejs st

---
+++ newapp/index.js
@@ -0,0 +1,5 @@
+const runfile = require("./newfile")
+function startapp(){
+    runfile()
+}
+startapp()
---
+++ newapp/newfile.js
@@ -0,0 +1,14 @@
+function runfile(){
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+    console.log("hi hello");
+
+}
+module.exports = runfile
